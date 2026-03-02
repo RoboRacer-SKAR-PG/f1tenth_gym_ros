01347f10045f921d5b8a0c86910575a4a11d8989
Ahmad Amine <ahmadamine998@gmail.com>
2026-03-02 06:01:30 -0500

    Add slam toolbox config for in-sim mapping

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch', 'slam'), glob('launch/slam/*.yaml')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'config', 'rviz'), glob('config/rviz/*.rviz')),
